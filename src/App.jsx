7efad00f741f4a17af8662fb0337d207a84e9581
Andrea Beggiato <andrea.beggiato@h-is.com>
2021-10-26 15:46:43 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './styles/index.scss';
+
+import Page from './pages/Page';
+
+const App = () => {
+  return (
+    <div>
+      <Page />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Page.jsx
@@ -0,0 +1,18 @@
+const Page = () => {
+  
+  return (
+    <div className="row">
+      <div className="col-2">
+        Col 1
+      </div>
+      <div className="col-8">
+        Col 2
+      </div>
+      <div className="col-2">
+        Col 3
+      </div>
+    </div>
+  );
+};
+
+export default Page;
