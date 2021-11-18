50821b46bb6fc9bfa5fe7100956a1172685e9569
Andrea Beggiato <andrea.beggiato@h-is.com>
2021-11-18 10:47:07 +0100

    empty category

--- src/components/products/ProductGrid.jsx
+++ src/components/products/ProductGrid.jsx
@@ -1,6 +1,12 @@
 const ProductGrid = (props) => {
   const products = props.products;
 
+  if (products.length === 0) {
+    return (
+      <h4>This category is empty!</h4>
+    );
+  }
+
   return products.map((product) => (
       <div className="row">
         <div className="col-12">
